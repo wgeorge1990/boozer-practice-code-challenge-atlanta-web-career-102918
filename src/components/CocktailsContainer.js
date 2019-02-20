0b3d6280c13b2fd37840021a82529694de52e17f
wgeorge1990 <williameverettgeorge@icloud.com>
2019-02-20 10:48:32 -0500

    Initial commit.

--- src/components/CocktailsContainer.js
+++ src/components/CocktailsContainer.js
@@ -3,12 +3,20 @@ import CocktailsList from './CocktailsList'
 import CocktailDisplay from './CocktailDisplay'
 import Form from './Form'
 
+const urlCocktails = "http://localhost:3000/sushis"
+
 class CocktailsContainer extends Component {
 
+fetchCocktails = () => {
+  fetch(urlCocktails)
+  .then(res => res.json())
+  .then(res => console.log())
+
+}
   render(){
     return (
       <div className="container">
-      
+
       </div>
     )
   }
